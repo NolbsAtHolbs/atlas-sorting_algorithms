47ab20b4a96054e01dd52501a2d3804d1abd04fd
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2024-10-02 22:38:22 -0500

    call limit

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,7 +1,9 @@
 #include "sort.h"
 
-static void merge_sort_recursive(int *array, size_t start, size_t end);
-static void merge(int *array, size_t start, size_t middle, size_t end);
+static void merge_sort_recursive(int *array, int *temp_array,
+	size_t start, size_t end);
+static void merge(int *array, int *temp_array, size_t start,
+	size_t middle, size_t end);
 
 /**
  * merge_sort - sorts an array of integers in ascending order using merge sort
@@ -10,75 +12,73 @@ static void merge(int *array, size_t start, size_t middle, size_t end);
  */
 void merge_sort(int *array, size_t size)
 {
+	int *temp_array;
+
 	if (!array || size < 2)
 		return;
-	merge_sort_recursive(array, 0, size - 1);
+	/* allocate temporary array once */
+	temp_array = malloc(sizeof(int) * size);
+	if (!temp_array)
+		return;
+	merge_sort_recursive(array, temp_array, 0, size - 1);
+	/* free the allocated memory */
+	free(temp_array);
 }
 
 /**
  * merge_sort_recursive - recursively divides the array and sorts the subarrays
  * @array: array of integers to sort
+ * @temp_array: temporary array for merging
  * @start: starting index of the subarray
  * @end: ending index of the subarray
  */
-static void merge_sort_recursive(int *array, size_t start, size_t end)
+static void merge_sort_recursive(int *array, int *temp_array,
+	size_t start, size_t end)
 {
 	size_t middle;
 
-	if (start < end)
-	{
-		middle = start + (end - start - 1) / 2;
-		merge_sort_recursive(array, start, middle);
-		merge_sort_recursive(array, middle + 1, end);
-		merge(array, start, middle, end);
-	}
+	if (start >= end)
+		return;
+	/* calculate the middle index */
+	middle = start + (end - start - 1) / 2;
+	merge_sort_recursive(array, temp_array, start, middle);
+	merge_sort_recursive(array, temp_array, middle + 1, end);
+	merge(array, temp_array, start, middle, end);
 }
 
 /**
  * merge - merges two sorted subarrays and prints the process
  * @array: array containing the subarrays to merge
+ * @temp_array: temporary array for merging
  * @start: starting index of the first subarray
  * @middle: ending index of the first subarray
  * @end: ending index of the second subarray
  */
-static void merge(int *array, size_t start, size_t middle, size_t end)
+static void merge(int *array, int *temp_array, size_t start,
+	size_t middle, size_t end)
 {
-	size_t left_size = middle - start + 1;
-	size_t right_size = end - middle;
-	int *left_array, *right_array;
-	size_t i = 0, j = 0, k = start;
+	size_t i = start, j = middle + 1, k = start;
 
-	left_array = malloc(sizeof(int) * left_size);
-	right_array = malloc(sizeof(int) * right_size);
-	if (!left_array || !right_array)
-	{
-		free(left_array);
-		free(right_array);
-		return;
-	}
 	printf("Merging...\n");
-	/* copy data to temporary arrays */
-	for (i = 0; i < left_size; i++)
-		left_array[i] = array[start + i];
 	printf("[left]: ");
-	print_array(left_array, left_size);
-	for (i = 0; i < right_size; i++)
-		right_array[i] = array[middle + 1 + i];
+	print_array(&array[start], middle - start + 1);
 	printf("[right]: ");
-	print_array(right_array, right_size);
-	/* merge the temporary arrays back into the original array */
-	i = 0;
-	j = 0;
-	while (k <= end)
+	print_array(&array[middle + 1], end - middle);
+	/* merge the subarrays into temp_array */
+	while (i <= middle && j <= end)
 	{
-		if (i < left_size && (j >= right_size || left_array[i] <= right_array[j]))
-			array[k++] = left_array[i++];
+		if (array[i] <= array[j])
+			temp_array[k++] = array[i++];
 		else
-			array[k++] = right_array[j++];
+			temp_array[k++] = array[j++];
 	}
+	while (i <= middle)
+		temp_array[k++] = array[i++];
+	while (j <= end)
+		temp_array[k++] = array[j++];
+	/* copy the merged elements back into the original array */
+	for (i = start; i <= end; i++)
+		array[i] = temp_array[i];
 	printf("[Done]: ");
 	print_array(&array[start], end - start + 1);
-
-	free(left_array);
-	free(right_array);
 }
